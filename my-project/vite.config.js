a8547a96fc9af43fc0aec28726258fdcfa7bfebf
codecraft-sanju <sanjusirvich18@gmail.com>
2026-03-01 00:56:55 +0530

    vite config

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  // Yahan se changes start ho rahe hain
+  build: {
+    chunkSizeWarningLimit: 1600, // Warning limit ko 1.6MB tak badha diya hai
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Isse har library ka apna alag chunk banega, bundle size balance karne ke liye
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 })
